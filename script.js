37fa51484f0a27ae7d149dd699e2968a383e59af
Hamza Ahmed Shad <35128191+HamzaAhmedShad@users.noreply.github.com>
2021-07-22 01:20:32 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let photosArray = [];
 
 // Unsplash API
 const count = 10;
-const apiKey = 'xkJd9SEBC1xeSHKAtxllWyOwflftOtakYbHToi9uuB4';
+const apiKey = 'APIkey';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Helper Function to Set Attributes on DOM Elements
